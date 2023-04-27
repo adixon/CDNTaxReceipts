ae5785ae504a9734c0793255b357ce9907cd1889
demeritcowboy <demeritcowboy@hotmail.com>
2023-04-26 22:48:49 -0400

    return an array on success and streamline error handling

--- api/v3/Cdntaxreceipts/Generate.php
+++ api/v3/Cdntaxreceipts/Generate.php
@@ -1,6 +1,12 @@
 <?php
 use CRM_Cdntaxreceipts_ExtensionUtil as E;
 
+define('CDNTAXRECEIPTS_API_NOTFOUND', 1);
+define('CDNTAXRECEIPTS_API_INELIGIBLE', 2);
+define('CDNTAXRECEIPTS_API_NOEMAIL', 3);
+define('CDNTAXRECEIPTS_API_NOTSENT', 4);
+define('CDNTAXRECEIPTS_API_LOGMISSING', 5);
+
 /**
  * Cdntaxreceipts.Generate API specification (optional)
  * This is used for documentation and validation.
@@ -31,10 +37,10 @@ function civicrm_api3_cdntaxreceipts_Generate($params) {
   $contribution->find(TRUE);
 
   if ($contribution->N === 0) {
-    return civicrm_api3_create_error('Contribution ID not found.');
+    return civicrm_api3_create_error('Contribution ID not found.', ['error_code' => CDNTAXRECEIPTS_API_NOTFOUND]);
   }
   if (!cdntaxreceipts_eligibleForReceipt($contribution->id)) {
-    return civicrm_api3_create_success(FALSE, $params, 'Cdntaxreceipts', 'Generate');
+    return civicrm_api3_create_error('Contribution not eligible.', ['error_code' => CDNTAXRECEIPTS_API_INELIGIBLE]);
   }
   // We don't support printing, so make sure they have an email.
   $email = new CRM_Core_DAO_Email();
@@ -42,9 +48,19 @@ function civicrm_api3_cdntaxreceipts_Generate($params) {
   $email->on_hold = 0;
   $email->find();
   if ($email->N === 0) {
-    return civicrm_api3_create_error('Contact does not have a valid email.');
+    return civicrm_api3_create_error('Contact does not have a valid email.', ['error_code' => CDNTAXRECEIPTS_API_NOEMAIL]);
   }
   $nullValue = NULL;
   [$sent] = cdntaxreceipts_issueTaxReceipt($contribution, $nullValue, CDNTAXRECEIPTS_MODE_API);
-  return civicrm_api3_create_success($sent, $params, 'Cdntaxreceipts', 'Generate');
+  if ($sent) {
+    // @todo There was some suggestion of providing true DAO support for these log tables. When that happens this could be converted.
+    $log = CRM_Core_DAO::executeQuery("SELECT lc.*, l.* FROM cdntaxreceipts_log_contributions lc INNER JOIN cdntaxreceipts_log l ON l.id = lc.receipt_id WHERE lc.contribution_id = %1", [1 => [$contribution->id, 'Integer']]);
+    if ($log->fetch()) {
+      return civicrm_api3_create_success([$log->id => $log->toArray()], $params, 'Cdntaxreceipts', 'Generate');
+    }
+    else {
+      return civicrm_api3_create_error('Unable to find/create log entry.', ['error_code' => CDNTAXRECEIPTS_API_LOGMISSING]);
+    }
+  }
+  return civicrm_api3_create_error('Error while sending email.', ['error_code' => CDNTAXRECEIPTS_API_NOTSENT]);
 }
--- tests/phpunit/api/v3/Cdntaxreceipts/GenerateTest.php
+++ tests/phpunit/api/v3/Cdntaxreceipts/GenerateTest.php
@@ -13,8 +13,10 @@ class api_v3_Cdntaxreceipts_GenerateTest extends CRM_Cdntaxreceipts_Base {
     $mut = new CiviMailUtils($this);
     $contact_id = $this->individualCreate(['first_name' => 'Tim', 'last_name' => 'Horton', 'email' => 'timmys@civicrm.org']);
     $contribution_id = $this->contributionCreate(['contact_id' => $contact_id]);
-    $result = civicrm_api3('Cdntaxreceipts', 'generate', ['contribution_id' => $contribution_id]);
+    $result = civicrm_api3('Cdntaxreceipts', 'generate', ['contribution_id' => $contribution_id, 'sequential' => 1]);
     $this->assertAPISuccess($result);
+    $this->assertEquals($contribution_id, $result['values'][0]['contribution_id']);
+    $this->assertEquals('email', $result['values'][0]['issue_method']);
     $mut->checkMailLog(['From: CDN Tax Org <cdntaxorg@example.org>', 'Subject: Your tax receipt C-00000001', 'Tim Horton', 'timmys@civicrm.org']);
   }
 
@@ -23,9 +25,17 @@ class api_v3_Cdntaxreceipts_GenerateTest extends CRM_Cdntaxreceipts_Base {
     $mut = new CiviMailUtils($this);
     $contact_id = $this->individualCreate(['first_name' => 'James', 'last_name' => 'Brown', 'email' => 'jbrown@civicrm.org']);
     $contribution_id = $this->contributionCreate(['contact_id' => $contact_id, 'total_amount' => 5, 'non_deductible_amount' => 5]);
-    $result = civicrm_api3('Cdntaxreceipts', 'generate', ['contribution_id' => $contribution_id]);
-    $this->assertAPISuccess($result);
-    $mut->assertMailLogEmpty();
+    $exceptionThrown = FALSE;
+    try {
+      $result = civicrm_api3('Cdntaxreceipts', 'generate', ['contribution_id' => $contribution_id]);
+    }
+    catch (CRM_Core_Exception $e) {
+      $exceptionThrown = TRUE;
+      $mut->assertMailLogEmpty();
+      $this->assertEquals('Contribution not eligible.', $e->getMessage());
+      $this->assertEquals(CDNTAXRECEIPTS_API_INELIGIBLE, $e->getErrorCode());
+    }
+    $this->assertTrue($exceptionThrown, 'Should have thrown an exception');
   }
 
   public function testNoContribution() {
@@ -33,9 +43,10 @@ class api_v3_Cdntaxreceipts_GenerateTest extends CRM_Cdntaxreceipts_Base {
     try {
       civicrm_api3('Cdntaxreceipts', 'generate', ['contribution_id' => 55555]);
     }
-    catch (Exception $e) {
+    catch (CRM_Core_Exception $e) {
       $exceptionThrown = TRUE;
       $this->assertEquals('Contribution ID not found.', $e->getMessage());
+      $this->assertEquals(CDNTAXRECEIPTS_API_NOTFOUND, $e->getErrorCode());
     }
     $this->assertTrue($exceptionThrown, 'Should have thrown an exception');
   }
@@ -51,9 +62,10 @@ class api_v3_Cdntaxreceipts_GenerateTest extends CRM_Cdntaxreceipts_Base {
     try {
       civicrm_api3('Cdntaxreceipts', 'generate', ['contribution_id' => $contribution_id]);
     }
-    catch (Exception $e) {
+    catch (CRM_Core_Exception $e) {
       $exceptionThrown = TRUE;
       $this->assertEquals('Contact does not have a valid email.', $e->getMessage());
+      $this->assertEquals(CDNTAXRECEIPTS_API_NOEMAIL, $e->getErrorCode());
     }
     $this->assertTrue($exceptionThrown, 'Should have thrown an exception');
 
@@ -64,9 +76,10 @@ class api_v3_Cdntaxreceipts_GenerateTest extends CRM_Cdntaxreceipts_Base {
     try {
       civicrm_api3('Cdntaxreceipts', 'generate', ['contribution_id' => $contribution_id]);
     }
-    catch (Exception $e) {
+    catch (CRM_Core_Exception $e) {
       $exceptionThrown = TRUE;
       $this->assertEquals('Contact does not have a valid email.', $e->getMessage());
+      $this->assertEquals(CDNTAXRECEIPTS_API_NOEMAIL, $e->getErrorCode());
     }
     $this->assertTrue($exceptionThrown, 'Should have thrown an exception');
   }
